05fc69a38d5f66933dabadcc1f96b3fb36cdba75
Eric Kim <eric@Erics-MacBook-Pro-3.local>
2015-11-07 03:59:27 -0500

    implemented login function

--- FroYo/AppDelegate.swift
+++ FroYo/AppDelegate.swift
@@ -8,12 +8,12 @@
 
 import UIKit
 import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
@@ -23,8 +23,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func application(application: UIApplication, openURL url: NSURL, sourceApplication: NSString, annotation: AnyObject) -> Bool {
-        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication as String, annotation: annotation)
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication!, annotation: annotation)
     }
 
     func applicationWillResignActive(application: UIApplication) {
---
+++ FroYo/LoginViewController.swift
@@ -0,0 +1,77 @@
+//
+//  LoginViewController.swift
+//  FroYo
+//
+//  Created by Eric Kim on 11/7/15.
+//  Copyright © 2015 EKEY. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class LoginViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        if FBSDKAccessToken.currentAccessToken() != nil {
+//            presentContactsViewController()
+//        }
+//        else {
+            let loginButton: FBSDKLoginButton = FBSDKLoginButton.init()
+            
+            loginButton.center = self.view.center
+            loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+            loginButton.delegate = self
+            
+            self.view.addSubview(loginButton)
+//        }
+    }
+    
+    func presentContactsViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let contactsViewController = storyboard.instantiateViewControllerWithIdentifier("ContactsViewController") as! ContactsViewController
+        
+        self.presentViewController(contactsViewController, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension LoginViewController: FBSDKLoginButtonDelegate {
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if error != nil {
+            print(error)
+        }
+        else if !result.isCancelled {
+            presentContactsViewController()
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        return
+    }
+    
+    func loginButtonWillLogin(loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+    
+}
