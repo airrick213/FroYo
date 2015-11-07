c58bfaf0fed36e6ec8c77211d25f40bb489fe2df
Edward Yoo <ehyoo97@gmail.com>
2015-11-07 02:25:40 -0500

    basic current user location gotten

--- FroYo/MapViewController.swift
+++ FroYo/MapViewController.swift
@@ -7,29 +7,28 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class MapViewController: UIViewController {
 
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+  @IBOutlet weak var mapView: MKMapView!
+  let locationManager = CLLocationManager()
+  
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+      locationManager.delegate = self
+      locationManager.requestAlwaysAuthorization()
+      
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
+  func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+    mapView.showsUserLocation = (status == .AuthorizedAlways)
+  }
 
 }
